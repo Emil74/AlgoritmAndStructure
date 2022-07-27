fd7185ab17259ffbf1e24030debe0414b189e2ef
Emil74 <emil_nagiyev_98@mail.ru>
2022-07-27 16:43:44 +0300

    NewWork_6

---
+++ Algoritm_HomeWork/Lesson6/Work6.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritm_HomeWork.Lesson6
+{
+    class Work6 : ILesson
+    {
+        public string Name => "6";
+
+        public string Description => "Реализовать загрузку списка уроков из файла.";
+
+        public void Run()
+        {
+            string userEnter;
+            var homeList = Louncher.GetList();
+            if (homeList.Length > 0)
+            {
+
+                do
+                {
+                    for (int i = 0; i < homeList.Length; i++)
+                    {
+                        Console.WriteLine($" {homeList[i]}");
+                    }
+                    Console.Write("Введите задание для выход -1");
+                    userEnter = Console.ReadLine();
+                    if (int.TryParse(userEnter, out int userChoice))
+                    {
+                        if (userChoice - 1 < homeList.Length && userChoice > 0)
+                        {
+                            Louncher.Run(userChoice - 1);
+                        }
+                    }
+
+                } while (userEnter != "-1");
+
+            }
+        }
+
+    }
+}
---
+++ HomeWorkk6/Louncher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Algoritm_HomeWork;
+namespace HomeWorkk6
+{
+    public class Louncher
+    {
+        private static List<ILesson> works = new List<ILesson>();
+        public static String[] GetList()
+        {
+            List<Type> homeWorkList = new List<Type>();
+            homeWorkList.AddRange(collection: Assembly.GetCallingAssembly().GetTypes().Where(t => t.GetInterfaces().Any(i => i == typeof(ILesson))));
+
+            List<string> tmp = new List<string>();
+
+            for (int i = 0; i < homeWorkList.Count; i++)
+            {
+                works.Add((ILesson)Activator.CreateInstance(homeWorkList[i]));
+                tmp.Add($"{works[i].Name} {works[i].Description}");
+            }
+            return tmp.ToArray();
+        }
+
+        public static void Run(int value)
+        {
+            works[value].Run();
+        }
+    }
+}
